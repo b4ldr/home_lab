b27e4e3465947fc25746833f6ed66634a8629b9f
John Bond <github@johnbond.org>
2022-08-18 21:03:14 +0200

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,5 @@ chown -R puppet /etc/puppet/code/production
 cd /etc/puppet/code/production || exit 1
 sudo -u puppet bundle install --path="${BUNDLE_PATH:-.bundle/vendor}"
 bundle exec r10k puppetfile install
-cp /etc/puppet/code/production/modules/puppet_apply/files/hiera.yaml /etc/puppet/hiera.yaml
+cp /etc/puppet/code/modules/puppet_apply/files/hiera.yaml /etc/puppet/hiera.yaml
 puppet apply  --modulepath=/etc/puppet/code/modules:/usr/share/puppet/modules:/etc/puppet/code/production/modules /etc/puppet/code/production/manifests
