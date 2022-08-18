89029461094d31b73f0eacd50190555ec5fb9fbe
John Bond <github@johnbond.org>
2022-08-18 20:54:38 +0200

    correct spelling

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install vim puppet git ruby-bundler
 git clone https://github.com/b4ldr/home_lab /etc/puppet/code/production
-chwon -R puppet /etc/puppet/code/production
+chown -R puppet /etc/puppet/code/production
 cd /etc/puppet/code/production || exit 1
 sudo -u puppet bundle install --path="${BUNDLE_PATH:-.bundle/vendor}"
 bundle exec r10k puppetfile install
