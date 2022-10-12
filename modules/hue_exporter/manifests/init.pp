74f4a6ab7ef4eafcf7a4ca75ee0fd4b5678f6f65
John Bond <github@johnbond.org>
2022-10-12 18:15:56 +0200

    cant download hue

--- modules/hue_exporter/manifests/init.pp
+++ modules/hue_exporter/manifests/init.pp
@@ -1,21 +1,11 @@
 # @sumary instal and manage hue-exporter
 # @param api_key the bridge api key
-# @param version the version to install
 # @param hue_bridge the hue bridge
 class hue_exporter (
   String       $api_key,
-  String       $version    = '1.0.0',
   Stdlib::Host $hue_bridge = 'localhost',
 ) {
-  $download_link = "https://github.com/SysdigDan/hue_exporter/releases/download/v${version}/hue_exporter_v${version}_Linux_x86_64.tar.gz"
-
-  archive { '/tmp/hue-exporter.tar.gz':
-    ensure       => present,
-    source       => $download_link,
-    extract      => true,
-    extract_path => '/usr/local/bin',
-    creates      => '/usr/local/bin/hue_exporter',
-  }
+  # build manually from https://github.com/aexel90/hue_exporter
   file { '/usr/local/bin/hue-exporter':
     ensure  => file,
     owner   => 'root',
