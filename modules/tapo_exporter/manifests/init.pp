f119e7c42299697b8e3e03df6ed556d1e2cd872a
John Bond <github@johnbond.org>
2022-11-26 01:56:03 +0100

    add tapo

--- modules/tapo_exporter/manifests/init.pp
+++ modules/tapo_exporter/manifests/init.pp
@@ -7,36 +7,47 @@ class tapo_exporter (
   String              $password,
   Array[Stdlib::Host] $plugs = []
 ) {
+  $cmd = '/usr/local/bin/tapo-exporter'
+  $git_root = '/srv/tapo_exporter'
+  $config_file = '/etc/tapo/config.yaml'
   $config = {
     email    => $email,
     password => $password,
     plugs    => $plugs,
   }
-  $cmd = '/usr/local/bin/tapo-exporter'
-  $git_root = '/srv/tapo_exporter'
+
   vcsrepo { $git_root:
     ensure   => present,
     provider => git,
     source   => 'https://github.com/b4ldr/tapo-exporter',
   }
+
   # build manually from https://github.com/aexel90/tapo_exporter
+  $script = @("SCRIPT")
+  #!/bin/bash
+  pushd ${git_root}
+  exec /usr/bin/python3 -m tapo_exporter.__init__ -c ${config_file}
+  SCRIPT
+
   file { '/usr/local/bin/tapo-exporter':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0555',
     cwd     => $git_root,
-    content => "#!/bin/bash\n/usr/bin/python3 -m tapo_exporter.__init__ -c ${config_file}",
+    content => $script,
     require => Vcsrepo[$git_root],
   }
+
   file { '/etc/tapo':
     ensure => directory,
   }
-  $config_file = '/etc/tapo/config.yaml'
+
   file { $config_file:
     ensure => file,
     config => $config.to_yaml,
   }
+
   $command = "${cmd} --config ${config_file}"
   systemd::manage_unit { 'tapo-exporter.service':
     enable        => true,
