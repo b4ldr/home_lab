b798a3514b4c237029714f2ee4737a5a15687cee
John Bond <github@johnbond.org>
2023-01-02 17:20:13 +0100

    add fw port

--- manifests/freya.pp
+++ manifests/freya.pp
@@ -5,13 +5,13 @@ node 'freya.home.arpa' {
     include my_fw::post
     include prometheus
     include prometheus::node_exporter
-    firewall {"101 prometheus":
+    firewall { '101 prometheus':
       proto  => 'tcp',
       dport  => 9090,
       action => 'accept',
     }
     include grafana
-    firewall {"102 grafana":
+    firewall { '102 grafana':
       proto  => 'tcp',
       dport  => 3000,
       action => 'accept',
@@ -24,4 +24,9 @@ node 'freya.home.arpa' {
     include blackbox_exporter
     include tapo_exporter
     # on storage https://github.com/ncabatoff/zfs-exporter
+    firewall { '103 plex':
+      proto  => 'tcp',
+      dport  => 32400,
+      action => 'accept',
+    }
 }
