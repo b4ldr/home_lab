391cf856e4db5ca5a35050a5310bbb62e7b88c8b
jbond <github@johnbond.org>
2024-06-21 18:30:19 +0200

    add fw rules

--- manifests/freya.pp
+++ manifests/freya.pp
@@ -10,12 +10,14 @@ node 'freya.home.arpa' {
     proto  => 'tcp',
     dport  => 9090,
     action => 'accept',
+    source => '192.168.1.0/24',
   }
   include grafana
   firewall { '102 grafana':
     proto  => 'tcp',
     dport  => 3000,
     action => 'accept',
+    source => '192.168.1.0/24',
   }
   include victron_exporter
   # TODO: create alarms for victron_alarm
@@ -25,11 +27,18 @@ node 'freya.home.arpa' {
   include blackbox_exporter
   include tapo_exporter
   include ecowitt_exporter
+  firewall { '105 ecowitt':
+    proto  => 'tcp',
+    dport  => 8080,
+    action => 'accept',
+    source => '192.168.1.0/24',
+  }
   # on storage https://github.com/ncabatoff/zfs-exporter
   firewall { '103 plex':
     proto  => 'tcp',
     dport  => 32400,
     action => 'accept',
+    source => '192.168.1.0/24',
   }
   include plex
 
