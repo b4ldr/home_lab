90af80908c06b6d519eaca5ba8abec147474943c
jbond <github@johnbond.org>
2024-06-21 16:39:43 +0200

    increase file limit

--- manifests/freya.pp
+++ manifests/freya.pp
@@ -1,33 +1,41 @@
 node 'freya.home.arpa' {
-    include puppet_apply
-    include my_fw::pre
-    include firewall
-    include my_fw::post
-    include prometheus
-    include prometheus::node_exporter
-    firewall { '101 prometheus':
-      proto  => 'tcp',
-      dport  => 9090,
-      action => 'accept',
-    }
-    include grafana
-    firewall { '102 grafana':
-      proto  => 'tcp',
-      dport  => 3000,
-      action => 'accept',
-    }
-    include victron_exporter
-    # TODO: create alarms for victron_alarm
-    include starlink_exporter
-    include hue_exporter
-    include snmp_exporter
-    include blackbox_exporter
-    include tapo_exporter
-    # on storage https://github.com/ncabatoff/zfs-exporter
-    firewall { '103 plex':
-      proto  => 'tcp',
-      dport  => 32400,
-      action => 'accept',
-    }
-    include plex
+  include puppet_apply
+  include my_fw::pre
+  include firewall
+  include my_fw::post
+  include prometheus
+  include prometheus::node_exporter
+  firewall { '101 prometheus':
+    proto  => 'tcp',
+    dport  => 9090,
+    action => 'accept',
+  }
+  include grafana
+  firewall { '102 grafana':
+    proto  => 'tcp',
+    dport  => 3000,
+    action => 'accept',
+  }
+  include victron_exporter
+  # TODO: create alarms for victron_alarm
+  include starlink_exporter
+  include hue_exporter
+  include snmp_exporter
+  include blackbox_exporter
+  include tapo_exporter
+  # on storage https://github.com/ncabatoff/zfs-exporter
+  firewall { '103 plex':
+    proto  => 'tcp',
+    dport  => 32400,
+    action => 'accept',
+  }
+  include plex
+
+  systemd::manage_dropin { 'limits.conf':
+    ensure        => present,
+    unit          => 'prometheus.service',
+    service_entry => {
+      'LimitNOFILE' => 49152,
+    },
+  }
 }
